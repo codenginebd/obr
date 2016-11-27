804ec7ac1d6b97282d3ce6091c8b82d2a4740a3f
codenginebd <codenginebd@gmail.com>
2016-11-27 01:21:01 -0500

    Added BookOrder and Payment models

--- order/models/book_order.py
+++ order/models/book_order.py
@@ -1,11 +1,15 @@
 from django.db import models
-
-from clients.models.client_user import ClientUser
 from generics.models.base_entity import BaseEntity
 from order.models.order_breakdown import BookOrderBreakdown
 
 
 class BookOrder(BaseEntity):
-    user = models.ForeignKey(ClientUser)
+    user = models.ForeignKey(User)
     breakdown = models.ManyToMany(BookOrderBreakdown)
-    status = models.IntegerField(default=0)
+    status = models.IntegerField(default=0) # placed, in progress, delivered
+    addresses = models.ManyToManyField(Address)
+    warehouse = models.ForeignKey(BookWarehouse)
+    delivered_by = models.ForeignKey(User, related_name='order_delivered_by')
+    payment_status = models.IntegerField(default=0) # Not Processed, Processed and Successful, Processed and Failed
+    payment = models.ForeignKey(PaymentTransaction, null=True)
+    payment_received_by = models.ForeignKey(User, related_name='payment_received_by')
--- order/models/order_breakdown.py
+++ order/models/order_breakdown.py
@@ -7,5 +7,6 @@ class BookOrderBreakdown(BaseEntity):
     book = models.ForeignKey(BookEdition)
     order_type = models.IntegerField(default=0) #1 for Sale, 0 for Rent
     quantity = models.IntegerField()
+    initial_payable_rent_price = models.DecimalField(max_digits=20, decimal_places=2)
     unit_price = models.DecimalField(max_digits=20, decimal_places=2)
     total_price = models.DecimalField(max_digits=20, decimal_places=2)
