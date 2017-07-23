557c8374a3c056dfd6f2db7363b1dfbfb4f282ef
Md Shariful Islam <codenginebd@gmail.com>
2017-07-23 21:33:38 +0600

    hosts.py updated

--- config/settings/hosts.py
+++ config/settings/hosts.py
@@ -1,1 +1,2 @@
-BR_ALLOWED_HOSTS = ["159.203.133.234", "127.0.0.1", "localhost", "bookozi.com", "develop.bookozi.com", "www.bookozi.com"]
+BR_ALLOWED_HOSTS = ["159.203.133.234", "127.0.0.1", "localhost",
+                    "bdreads.com", "www.bdreads.com", "develop.bdreads.com"]
