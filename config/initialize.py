4876d07b8c52a9d20e731f78ce11161547d2dfb3
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-07-30 06:14:46 +0000

    initialize.py created online with Bitbucket

---
+++ config/initialize.py
@@ -0,0 +1,45 @@
+# Initialize Book Languages
+from book_rental.models.language import BookLanguage
+
+BOOK_LANGUAGES = [ 
+{ 
+    'short_name': 'en',
+    'long_name': 'English'
+},
+{
+    'short_name': 'bn',
+    'long_name': 'Bangla'
+}
+]
+
+def init_book_languages():
+    for lang in Book_LANGUAGES:
+        blang_objects = BookLanguage.objects.filter(short_name=lang['short_name')
+        if blang_objects.exists():
+            blang_object = blang_objects.first()
+        else:
+            blang_object = BookLanguage()
+            
+        blang_object.name = lang['long_name']
+        blang_object.save()
+        
+init_book_languages()
+
+# Initialize Book Rental Plans
+from generic.models.sales.rent_plan import RentPlan
+
+RENT_DAYS = [ 7, 10, 15, 20, 30, 45, 60, 75, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360 ]
+
+RENT_PLANS = []
+
+for rday in RENT_DAYS:
+    name = '%s_DAY_PLAN' % rday
+    rent_plan_objects = RentPlan.objects.filter(days=rday)
+    if rent_plan_objects.exists():
+        rent_plan_object = rent_plan_objects.first()
+    else:
+        rent_plan_object = RentPlan(days=rday)
+    rent_plan_object.name = name
+    rent_plan_object.save()
+
+
