0663f73d96ae4585d0d3f1485b778c43e91627e1
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-07-30 07:52:53 +0000

    error_log.py edited online with Bitbucket

--- logger/models/error_log.py
+++ logger/models/error_log.py
@@ -6,3 +6,11 @@ from generics.models.base_entity import BaseEntity
 class ErrorLog(BaseEntity):
     url = models.CharField(max_length=500)
     stacktrace = models.TextField(blank=True, null=True)
+    
+    @classmethod
+    def log(cls, url, stacktrace=None):
+        instance = cls()
+        instance.url = url
+        if stacktrace:
+            instance.stacktrace = stacktrace
+        instance.save()
