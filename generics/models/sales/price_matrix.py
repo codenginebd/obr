2191f8ad95e80c562e2e729e4707c3147373a489
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-07-31 07:30:01 +0000

    price_matrix.py edited online with Bitbucket

--- generics/models/sales/price_matrix.py
+++ generics/models/sales/price_matrix.py
@@ -5,7 +5,7 @@ from generics.models.sales.rent_plan import RentPlan
 
 
 class PriceMatrix(BaseEntity):
-    plans = models.ManyToManyField(RentPlan)
+    rent_plans = models.ManyToManyField(RentPlan)
     is_rent = models.BooleanField(default=False)
     offer_price_p = models.DecimalField(max_digits=20, decimal_places=2, default=0)  # value in percentage
     offer_price_v = models.DecimalField(max_digits=20, decimal_places=2, default=0)  # value in value
