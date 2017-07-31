7848ee7fe63445252c54447207294ab01f5d9c88
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-07-31 11:41:41 +0000

    publisher_downloader.py created online with Bitbucket

---
+++ book_rental/libs/downloader/publisher_downloader.py
@@ -0,0 +1,18 @@
+from book_rental.libs.downloader.downloader import Downloader
+from engine.exceptions.br_exception import BRException
+from generics.libs.writer.excel_file_writer import ExcelFileWriter
+from generics.libs.writer.writter import Writter
+
+
+class PublisherDownloader(Downloader):
+
+    def __init__(self, file_name, *args, **kwargs):
+        super(PublisherDownloader, self).__init__(file_name=file_name, *args, **kwargs)
+
+    def get_default_writer(self):
+        return ExcelFileWriter
+
+    def get_header_names(self):
+        return [
+            "Code", "Publisher Name", "Publisher Description", "Publisher Image", "Email(s)", "Phone(s)"
+        ]
