4f20da4c80ab1e75710456d8a6397d825f209157
Md Shariful Islam <codenginebd@gmail.com>
2017-08-05 01:02:44 +0600

    Worked on book browse categories

--- book_rental/mixin/common_data_mixin.py
+++ book_rental/mixin/common_data_mixin.py
@@ -6,5 +6,8 @@ class CommonDataMixin(object):
     def get_all_categories(self):
         return ProductCategory.get_all_children()
 
-    def get_all_parent_categiries(self):
+    def get_all_parent_categories(self):
         return ProductCategory.get_all_parent_categories()
+
+    def get_all_children(self, cat_id=None, **kwargs):
+        return ProductCategory.get_all_children(cat_id=cat_id, **kwargs)
--- book_rental/views/browse_view.py
+++ book_rental/views/browse_view.py
@@ -12,15 +12,44 @@ class BookBrowseView(BaseTemplateView, CommonDataMixin):
 
         slug = kwargs.get('slug')
 
+        category_object = None
+
         if slug:
             slug = slug.split('/')[-1:]
             if slug:
                 slug = slug[0]
 
-        if not slug:
-            context["filter_categories"] = self.get_all_parent_categiries()
+                category_objects = ProductCategory.objects.filter(slug=slug)
+                category_object = category_objects.first()
 
-        book_category_objects = ProductCategory.objects.filter(slug=slug)
+        if not slug:
+            context["filter_category_show"] = True
+            context["linkable"] = False
+            context["filter_category_header"] = "All Categories"
+            childrens = self.get_all_parent_categories()
+            context["filter_categories"] = childrens
+            if len(childrens) > 10:
+                context["show_search"] = True
+            else:
+                context["show_search"] = False
+        else:
+            if category_object:
+                context["filter_category_show"] = True
+                context["linkable"] = True
+                context["slug"] = category_object.slug
+                context["filter_category_header"] = category_object.name
+                childrens = self.get_all_children(cat_id=category_object.pk)['children']
+                if childrens:
+                    context["filter_categories"] = childrens
+                else:
+                    context["filter_category_show"] = False
+
+                if len(childrens) > 10:
+                    context["show_search"] = True
+                else:
+                    context["show_search"] = False
+            else:
+                context["filter_category_show"] = False
 
         context['all_categories'] = self.get_all_categories()
 
