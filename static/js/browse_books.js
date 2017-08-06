598bcf01346723aea981f3470968c60ce2499f4f
Md Shariful Islam <codenginebd@gmail.com>
2017-08-06 23:57:48 +0600

    Added login/register menu

--- static/js/browse_books.js
+++ static/js/browse_books.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
         $(this).addClass("browse_item_hover");
         $(this).parent(".book_entry").addClass("browse_item_hover_color");
         $(this).find(".book_entry_action").removeClass("displaynone");
-        $(this).find(".action-bar").slideDown("slow");
+        $(this).find(".action-bar").slideDown("fast");
     }, function (e) {
         $(this).removeClass("browse_item_hover");
         $(this).parent(".book_entry").removeClass("browse_item_hover_color");
         $(this).find(".book_entry_action").addClass("displaynone");
-        $(this).find(".action-bar").slideUp("slow");
+        $(this).find(".action-bar").slideUp("fast");
     });
 
     $(".product-quick-view").click(function (e) {
