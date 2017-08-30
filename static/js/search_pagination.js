a4d760840d54dc9d424e018200c60cec4e26af13
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-08-30 05:04:07 +0000

    search_pagination.js created online with Bitbucket

---
+++ static/js/search_pagination.js
@@ -0,0 +1,10 @@
+(function(){
+
+    window.create_pagination_object = function(total_objects, page_size, prev, next) {
+        
+    };
+
+    $(document).ready(function() {
+        
+    });
+})();
