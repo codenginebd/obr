814c4631a4e5e49926ca72c7765ac9691de34f81
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-08-31 08:06:31 +0000

    category_serializer.py edited online with Bitbucket

--- ecommerce/api/serializers/category_serializer.py
+++ ecommerce/api/serializers/category_serializer.py
@@ -1,9 +1,14 @@
+from django.core.urlresolvers import reverse
 from ecommerce.models.sales.category import ProductCategory
 from generics.api.serializers.base_model_serializer import BaseModelSerializer
-
+from rest_framework import serializers
 
 class CategorySerializer(BaseModelSerializer):
+    link = serializers.SerializerMethodField()
+    
+    def get_link(self, obj):
+        return ""
 
     class Meta:
         model = ProductCategory
-        fields = ('id', 'code', 'name', 'parent_id', 'date_created', 'last_updated')
+        fields = ('id', 'code', 'name', 'link', 'parent_id', 'date_created', 'last_updated')
