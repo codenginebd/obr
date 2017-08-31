3aa830be31c1d5e85bdf5025a2dcb7f4ac54fb2a
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-08-31 08:08:16 +0000

    author_serializer.py edited online with Bitbucket

--- book_rental/api/serializers/author_serializer.py
+++ book_rental/api/serializers/author_serializer.py
@@ -1,8 +1,15 @@
+from django.core.urlresolvers import reverse
 from book_rental.models.author import Author
 from generics.api.serializers.base_model_serializer import BaseModelSerializer
+from rest_framework import serializers
 
 
 class AuthorSerializer(BaseModelSerializer):
+    link = serializers.SerializerMethodField()
+    
+    def get_link(self, obj):
+        return ""
+
     class Meta:
         model = Author
         fields = ('id', 'code', 'name', 'date_created', 'last_updated')
