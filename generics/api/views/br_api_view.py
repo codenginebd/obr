396ce40ce22db9944f2fa25297d07f9633dfaaae
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-05 08:40:20 +0000

    br_api_view.py edited online with Bitbucket

--- generics/api/views/br_api_view.py
+++ generics/api/views/br_api_view.py
@@ -1,4 +1,30 @@
 from rest_framework.views import APIView
 
 class BRAPIView(APIView):
-    pass
+    
+    def get_queryset(self, **kwargs):
+        return None
+        
+    def filter_criteria(self, request, queryset):
+        return queryset
+        
+    def get_many(self):
+        return False
+        
+    def get_serializer_class(self, request, queryset, **kwargs):
+        return None
+        
+    def create_response(self, request, queryset):
+        return {}
+
+    def get(self, request, format=None):
+        queryset = self.get_queryset(**kwargs)
+        queryset = self.filter_criteria(request, queryset)
+        created_response = self.create_response(request, queryset)
+        if created_response:
+            return Response(created_response)
+        else:
+            many = self.get_many()
+            serializer_class = self.get_serializer_class()
+            rendered_response = serializer_class(queryset=queryset, many=many)
+            return Response(rendered_response)
