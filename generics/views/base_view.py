74a423a827501dfc72565850d10454dda6fcd185
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-06 06:22:29 +0000

    base_view.py edited online with Bitbucket

--- generics/views/base_view.py
+++ generics/views/base_view.py
@@ -2,4 +2,17 @@ from django.views.generic.base import View
 
 
 class BaseView(View):
-    pass
+
+    def get_context_data(self, request, *args, **kwargs):
+        return {}
+        
+    def prepare_response(self, request, context, *args, **kwargs):
+        return {}
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(request, *args, **kwargs)
+        response = self.prepare_response(request, context, *args, **kwargs)
+        return response
+        
+    def post(self, request, *args, **kwargs):
+        pass
