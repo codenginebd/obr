9a7b14dbcc251ae5307599e9ca86699a4b06117a
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-06 06:05:01 +0000

    shopping.py edited online with Bitbucket

--- ecommerce/views/shopping.py
+++ ecommerce/views/shopping.py
@@ -1,6 +1,6 @@
+from generics.views.base_template_view import BaseTemplateView
 
-
-class AddToCartView(View):
+class AddToCartView(BaseTemplateView):
 
     def get(self, request, *args, **kwargs):
         pass
