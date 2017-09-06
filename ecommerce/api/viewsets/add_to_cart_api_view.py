77ffd21aa26c565180d536a45446005f41d48f2c
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-06 09:27:44 +0000

    add_to_cart_api_view.py edited online with Bitbucket

--- ecommerce/api/viewsets/add_to_cart_api_view.py
+++ ecommerce/api/viewsets/add_to_cart_api_view.py
@@ -2,6 +2,12 @@ from generics.api.views.br_api_view import BRAPIView
 
 class AddToCartAPIView(BRAPIView):
 
+    def handle_buy_to_cart(self, product_type, product_code, is_new, print_type, **kwargs):
+        pass
+        
+    def handle_rent_to_cart(self, product_type, product_code, is_new, print_type, rent_days, **kwargs):
+        pass 
+
     def handle_post(self, request):
         product_type = request.query_params.get('ptype')
         product_code = request.query_params.get('pcode')
@@ -11,11 +17,29 @@ class AddToCartAPIView(BRAPIView):
         buy_type = request.query_params.get('buy-type') # buy or rent
         if buy_type == 'rent':
             rent_days = request.query_params.get('days')
+            rent_days = int(rent_days)
+            self.handle_rent_to_cart(product_type, product_code, is_new, print_type, rent_days)
+        else:
+            self.handle_buy_to_cart(product_type, product_code, is_new, print_type)
+            
+                
+    def is_valid(self, request):
+        product_type = request.query_params.get('ptype')
+        product_code = request.query_params.get('pcode')
+        used = request.query_params.get('used')
+        is_new = True if not used else False
+        print_type = request.query_params.get('pr-type')
+        buy_type = request.query_params.get('buy-type') # buy or rent
+        valid = True
+        if any([ not product_type, not product_code, not used, not print_type, not buy_type ]):
+            valid = False
+        if buy_type == 'rent':
+            rent_days = request.query_params.get('days')
             try:
                 rent_days = int(rent_days)
-                
             except Exception as exp:
-                pass
+                valid = False
+        return valid
         
     def create_post_response(self, request, data, *args, **kwargs):
         return {}
