f404e8c01e41dbcfcdd6107f7611c701dd54d38c
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-07 05:59:15 +0000

    cart.py edited online with Bitbucket

--- ecommerce/cart.py
+++ ecommerce/cart.py
@@ -12,20 +12,17 @@ Cart Structure
         1213: 
         {
             'buy': 
-            [
-                {
+            {
                     'qty': 10,
                     'unit_price': 100,
                     'subtotal': 1000,
+                    'promo_applied': True,
                     'promo_code': 'ZacdaSds',
-                    'promo_amount': 200,
                     'discount_applied': True,
-                    'discount_offer_type': 'amount',
-                    'discount_amount': 300,
+                    'discount_code': 'DSdsDcD',
                     'total': 500,
                     'currency_code': 'BDT'
-                }
-            ],
+            },
             'rent': 
             [
                 {
@@ -33,15 +30,22 @@ Cart Structure
                     'unit_price': 100,
                     'subtotal': 1000,
                     'rent_days': 30,
+                    'promo_applied': True,
                     'promo_code': 'ZacdaSds',
-                    'promo_amount': 200,
                     'discount_applied': True,
-                    'discount_offer_type': 'amount',
-                    'discount_amount': 300,
+                    'discount_code': 'DSdsDcD',
                     'total': 500,
                     'currency_code': 'BDT'
                 }
-            ]
+            ],
+            'sale': 
+            {
+                    'qty': 10,
+                    'unit_price': 100,
+                    'subtotal': 1000,
+                    'total': 500,
+                    'currency_code': 'BDT'
+            }
         } 
     }
 }
@@ -56,12 +60,36 @@ class Cart(object):
     def __init__(self, request, *args, **kwargs):
         if not settings.CART_SESSION_ID in request.session:
             request.session[settings.CART_SESSION_ID] = {}
+        self.cart = request.session[settings.CART_SESSION_ID]
         self.request = request
         
-    def add_to_cart(self, buy_type, product_code, qty, unit_price, promo_applied):
+    def add_to_cart(self, buy_type, product_code, qty, unit_price, promo_applied, promo_code, discount_applied, discount_code, currency_code):
         product_objects = Book.objects.filter(code=product_code)
         if product_objects.exists():
             product_object = product_objects.first()
+            if not 'items' in self.cart.items():
+                self.cart['items'] = {}
+                
+            if not product_object.pk in self.cart['items'].items():
+                self.cart['items'][product_object.pk] = { 'buy': {  }, 'rent': [ ], 'sale': { } }
+                
+            if buy_type == 'buy':
+                product_buy_cart = self.cart['items'][product_object.pk]['buy']
+                
+                self.cart['items'][product_object.pk]['buy'] = product_buy_cart
+            elif buy_type == 'rent':
+                product_rent_cart = self.cart['items'][product_object.pk]['rent']
+                
+                self.cart['items'][product_object.pk]['rent'] = product_rent_cart
+            elif buy_type == 'sale':
+                product_sale_cart = self.cart['items'][product_object.pk]['sale']
+                
+                self.cart['items'][product_object.pk]['sale'] = product_sale_cart
+            
+    def save(self):
+        self.cart['last_modified'] = datetime.utcnow()
+        self.request.session[settings.CART_SESSION_ID] = self.cart
+            
             
         
         
