a29c90f6e232c5d9a5c35a8aeca52eac8ed5d3ad
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-07 12:34:00 +0000

    promotion.py edited online with Bitbucket

--- promotion/models/promotion.py
+++ promotion/models/promotion.py
@@ -2,6 +2,7 @@ from django.db import models
 from generics.models.base_entity import BaseEntity
 from promotion.models.promotion_reward import PromotionReward
 from promotion.models.promotion_rule import PromotionRule
+from promotion.model_managers import PromotionManagerByQuantity, PromotionManagerByAmount, PromotionManagerByProducts
 
 
 class Promotion(BaseEntity):
@@ -11,3 +12,10 @@ class Promotion(BaseEntity):
     rewards = models.ManyToManyField(PromotionReward)
     start_date = models.BigIntegerField(default=0)
     end_date = models.BigIntegerField(default=0)
+    
+    objects_by_quantity = PromotionManagerByQuantity()
+    objects_by_amount = PromotionManagerByAmount()
+    objects_by_products = PromotionManagerByProducts()
+    
+    
+    
