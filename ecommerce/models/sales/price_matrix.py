973cb0748ce3c47ea3af37ab218d7a405d909bd3
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-08 05:22:48 +0000

    price_matrix.py edited online with Bitbucket

--- ecommerce/models/sales/price_matrix.py
+++ ecommerce/models/sales/price_matrix.py
@@ -1,9 +1,19 @@
 from django.db import models
+from datetime import datetime
 from ecommerce.models.rent_plan import RentPlan
 from ecommerce.models.sales.rent_plan_relation import RentPlanRelation
 from generics.models.base_entity import BaseEntity
 from payment.models.currency import Currency
 
+class PriceMatrixManager(models.Manager):
+    def get_queryset(self):
+    
+        todays_datetime = datetime.now()
+        todays_ts = Clock.convert_datetime_to_utc_timestamp(todays_datetime)
+    
+        queryset = super(PriceMatrixManager, self).get_queryset()
+        queryset = queryset.filter(Q(is_rent=False) | (Q(is_rent=True) & Q(offer_date_start__isnull=False) & Q(offer_date_start__lte=todays_ts) & Q(offer_date_end__isnull=False) & Q(offer_date_end__gte=todays_ts)))
+        return queryset
 
 class PriceMatrix(BaseEntity):
     rent_plans = models.ManyToManyField(RentPlan, through=RentPlanRelation)
@@ -21,6 +31,8 @@ class PriceMatrix(BaseEntity):
     market_price = models.DecimalField(max_digits=20, decimal_places=2)
     sale_price = models.DecimalField(max_digits=20, decimal_places=2, null=True)
     currency = models.ForeignKey(Currency)
+    
+    objects = PriceMatrixManager()
     
     class Meta:
         index_together = [ 'product_code', 'product_model', 'is_new', 'print_type' ]
