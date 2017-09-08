a20616b8698d7247edb0b9516cec11af375d32d1
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-08 04:34:49 +0000

    promotion_manager.py edited online with Bitbucket

--- promotion/promotion_manager.py
+++ promotion/promotion_manager.py
@@ -2,10 +2,10 @@
 
 class PromotionManager(object):
 
-    def __init__(self, cart, **kwargs):
-        self.cart = cart
+    def __init__(self, **kwargs):
         self.kwargs = kwargs
         
-    def apply_promotion(self):  # return is_promotion_applied, promotion_code
+    def apply_promotion(self, cart):  # return is_promotion_applied, promotion_code
+        self.cart = cart
         return False, None
         
