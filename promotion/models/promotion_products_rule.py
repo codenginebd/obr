5dd27c72326024ecb169f043828157fa7878213d
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-11 09:32:46 +0000

    promotion_products_rule.py edited online with Bitbucket

--- promotion/models/promotion_products_rule.py
+++ promotion/models/promotion_products_rule.py
@@ -6,4 +6,4 @@ class PromotionProductRule(BaseEntity):
     product_id = models.BigIntegerField(null=True)
     product_model = models.CharField(max_length=200, null=True)
     min_qty = models.IntegerField(default=0)
-    min_amount = models.DecimalField(max_digits=20, decimal_places=2)
+    min_amount = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
