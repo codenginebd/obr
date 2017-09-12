82b1cb868b8ecf65e68a1548cdffdd360e907eb5
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-12 10:55:08 +0000

    wallet_transaction.py edited online with Bitbucket

--- payment/models/wallet_transaction.py
+++ payment/models/wallet_transaction.py
@@ -1,4 +1,21 @@
-
+from django.db import models
 
 class WalletTransaction(BaseEntity):
-    pass
+    wallet = models.ForeignKey(PaymentWallet)
+    transaction_type = models.IntegerField(default=TRANSACTION_TYPES.CREDIT_STORE.value)
+    total = models.DecimalField(decimal_places=2, max_digits=20)
+    transaction_status = models.IntegerField(default=PAYMENT_STATUS.PENDING.value)
+    
+    
+    @classmethod
+    def create_wallet_transaction(cls, wallet_id, transaction_type, total, transaction_status):
+        tnx_instance = cls()
+        tnx_instance.wallet_id = wallet_id
+        tnx_instance.transaction_type = transaction_type
+        tnx_instance.total = total
+        tnx_instance.transaction_status = transaction_status
+        tnx_instance.save()
+        return tnx_instance
+        
+        
+        
