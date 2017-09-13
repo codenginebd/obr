72cf41f63bb41d561e0c7e032a8708ff1acb4679
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-13 11:50:49 +0000

    promotion_products_rule.py edited online with Bitbucket

--- promotion/models/promotion_products_rule.py
+++ promotion/models/promotion_products_rule.py
@@ -5,5 +5,7 @@ from generics.models.base_entity import BaseEntity
 class PromotionProductRule(BaseEntity):
     product_id = models.BigIntegerField(null=True)
     product_model = models.CharField(max_length=200, null=True)
+    is_new = models.BooleanField(default=True)
+    print_type = models.CharField(max_length=100, null=True)
     min_qty = models.IntegerField(default=0)
     min_amount = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
