972d20c01ad36dbe7d7847252e14d81d63176507
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-13 07:26:09 +0000

    update_promotions.py edited online with Bitbucket

--- book_rental/management/commands/update_promotions.py
+++ book_rental/management/commands/update_promotions.py
@@ -10,21 +10,60 @@ class Command(BaseCommand):
         Promotion.objects.all().delete()
 
         print("Starting...")
-        start_date = datetime.utcnow().date()
-        end_date = start_date + timedelta(days=5)
-        instance = Promotion.create_or_update_promotion(title="Test Promo",
-                                                        description="Test Promo Desc",
-                                                        start_date=start_date, end_date=end_date,
-                                                        promotion_type="BUY",
-                                                        by_cart_products_dates="BY_CART",
-                                                        by_amount_qty="BY_AMOUNT",
-                                                        min_qty_amount=100,
-                                                        products=[(1,"Book", 50)],
-                                                        rewards=[(PromotionRewardTypes.AMOUNT_IN_MONEY.value,
-                                                                 False, 200, None, None, [(1,"Book", 50)])])
+        # Buy 1 get 1 for product id 1
+        start_date = datetime.utcnow().date() + timedelta(days=5)
+        end_date = start_date + timedelta(days=10)
+        instance = Promotion.create_or_update_promotion(title="Buy 1 Get 1 for Product 1",
+		        description="This offer is for product 1 only. Buy 1 item and get 1 item free",
+		        start_date=start_date, end_date=end_date,
+		        promotion_type="BUY",
+		        by_cart_products_dates="BY_PRODUCTS",
+		        by_amount_qty="BY_QTY",
+		        min_qty_amount=1,
+		        products=[(1,"Book", 1)],
+		        rewards=[(PromotionRewardTypes.FREE_PRODUCTS.value,
+				         None, None, None, None, [(1,"Book", 1)])])
+				 
         if instance:
-            print("Created.")
-            print(instance.pk)
+	        print("Created.")
+	        print(instance.pk)
+				 
+        # Buy min 500 BDT and get free shipping				 
+        start_date = datetime.utcnow().date()
+        end_date = start_date + timedelta(days=30)
+        instance = Promotion.create_or_update_promotion(title="Buy min 500 BDT and get free shipping	",
+		        description="Buy min 500 BDT and get free shipping	",
+		        start_date=start_date, end_date=end_date,
+		        promotion_type="BUY",
+		        by_cart_products_dates="BY_CART",
+		        by_amount_qty="BY_AMOUNT",
+		        min_qty_amount=500,
+		        products=[],
+		        rewards=[(PromotionRewardTypes.FREE_SHIPPING.value,
+				         None, None, None, None, [])])
+				 
+        if instance:
+	        print("Created.")
+	        print(instance.pk)
+				 
+				 
+         # Buy product 2 min 3 items and get 1 item free
+         start_date = datetime.utcnow().date() + timedelta(days=1)
+         end_date = start_date + timedelta(days=10)
+         instance = Promotion.create_or_update_promotion(title="Buy product 2 min 3 items and get 1 item free",
+		         description="Buy product 2 min 3 items and get 1 item free",
+		         start_date=start_date, end_date=end_date,
+		         promotion_type="BUY",
+		         by_cart_products_dates="BY_PRODUCTS",
+		         by_amount_qty="BY_QTY",
+		         min_qty_amount=1,
+		         products=[(2,"Book", 3)],
+		         rewards=[(PromotionRewardTypes.FREE_PRODUCTS.value,
+                    None, None, None, None, [(2,"Book", 1)])])
+				 
+        if instance:
+	        print("Created.")
+	        print(instance.pk)
         for p in Promotion.objects.all():
             print(p.title)
         print("Ended.")
