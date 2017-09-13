6cab9cabf370ad0a37ca59388b48c3c3992f1171
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-13 11:51:16 +0000

    promotion_reward_products.py edited online with Bitbucket

--- promotion/models/promotion_reward_products.py
+++ promotion/models/promotion_reward_products.py
@@ -5,4 +5,6 @@ from generics.models.base_entity import BaseEntity
 class PromotionRewardProduct(BaseEntity):
     product_id = models.BigIntegerField(null=True)
     product_model = models.CharField(max_length=200, null=True)
+    is_new = models.BooleanField(default=True)
+    print_type = models.CharField(max_length=100, null=True)
     quantity = models.IntegerField(default=1)
