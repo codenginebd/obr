58960cec79383a6ab3f72e6c2f66ab378068210a
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-19 09:20:16 +0000

    inventory.py edited online with Bitbucket

--- inventory/models/inventory.py
+++ inventory/models/inventory.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from ecommerce.models.sales.warehouse import Warehouse
 from generics.models.base_entity import BaseEntity
 
@@ -9,7 +8,9 @@ class Inventory(BaseEntity):
     product_model = models.CharField(max_length=200)
     warehouse = models.ForeignKey(Warehouse)
     stock = models.BigIntegerField(default=0)
+    available_for_buy = models.BooleanField(default=False)
     available_for_rent = models.BooleanField(default=False)
+    available_for_sale = models.BooleanField(default=False)
     is_new = models.IntegerField(default=0)
     print_type = models.CharField(max_length=50) # COL, ORI, ECO #Color, Original and Economy
     comment = models.TextField(null=True)
