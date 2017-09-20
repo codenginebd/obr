2a400f41cf996bf7e6b92f4591d2d2f4d7e27551
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-20 06:54:58 +0000

    base_list_view.py edited online with Bitbucket

--- bradmin/views/base_list_view.py
+++ bradmin/views/base_list_view.py
@@ -2,6 +2,7 @@ from django.views.generic.list import ListView
 
 
 class BaseListView(ListView):
+    paginate_by = 15
 
     def apply_filter(self, queryset):
         return queryset
@@ -20,13 +21,23 @@ class BaseListView(ListView):
     def get_headers(self):
         return []
 
-    def get_table_data(self):
+    def prepare_table_data(self, queryset):
         return []
 
     def get_context_data(self, **kwargs):
         context = super(BaseListView, self).get_context_data(**kwargs)
+        paginator = Paginator(self.get_queryset(), self.paginate_by)
+        page = self.request.GET.get('page')
+        try:
+            object_list = paginator.page(page)
+        except PageNotAnInteger:
+            object_list = paginator.page(1)
+        except EmptyPage:
+            object_list = paginator.page(paginator.num_pages)
+
+        context['list_exams'] = file_exams
         context["breadcumb"] = self.get_breadcumb()
         context["left_menu_items"] = self.get_left_menu_items()
         context["headers"] = self.get_headers()
-        context["table_data"] = self.get_table_data()
+        context["table_data"] = self.prepare_table_data(queryset=object_list)
         return context
