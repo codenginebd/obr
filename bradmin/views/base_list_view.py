76c2db5779ea19483bf2f425d22e4653adfdef57
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-09-20 08:26:32 +0000

    base_list_view.py edited online with Bitbucket

--- bradmin/views/base_list_view.py
+++ bradmin/views/base_list_view.py
@@ -18,11 +18,14 @@ class BaseListView(ListView):
     def get_left_menu_items(self):
         return {}
 
-    def get_headers(self):
+    def get_table_headers(self):
         return []
 
     def prepare_table_data(self, queryset):
         return []
+        
+    def get_extra_context(self, request, queryset):
+        return {}
 
     def get_context_data(self, **kwargs):
         context = super(BaseListView, self).get_context_data(**kwargs)
@@ -34,10 +37,16 @@ class BaseListView(ListView):
             object_list = paginator.page(1)
         except EmptyPage:
             object_list = paginator.page(paginator.num_pages)
-
+        self.object_list = object_list
         context['list_exams'] = file_exams
         context["breadcumb"] = self.get_breadcumb()
         context["left_menu_items"] = self.get_left_menu_items()
         context["headers"] = self.get_headers()
         context["table_data"] = self.prepare_table_data(queryset=object_list)
+        extra_context = self.get_extra_context(request=self.request, queryset=object_list)
+        for key, item in extra_context.items():
+            if key not in context.keys():
+                context[key] = item
+            else:
+                ErrorLog.log(url='', stacktrace='%s cannot be used as key as it is already in use in the parent context', context='%s' % self.model.__name__)
         return context
