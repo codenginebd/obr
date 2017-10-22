3045d58fc7b3a66c47b3b7da106a2e90b2910fa7
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-10-22 07:20:10 +0000

    admin_action_button_mixin.py edited online with Bitbucket

--- bradmin/mixins/admin_action_button_mixin.py
+++ bradmin/mixins/admin_action_button_mixin.py
@@ -6,33 +6,63 @@ from bradmin.enums import ViewAction
 class AdminActionButtonMixin(object):
     def show_upload(self):
         return self.model.show_upload()
+        
+    def show_upload_as(self):
+        return self.model.show_upload_as()
 
     def show_download(self):
         return self.model.show_download()
+        
+    def show_download_as(self):
+        return self.model.show_download_as()
 
     def show_download_template(self):
         return self.model.show_download_template()
+        
+    def show_download_template_as(self):
+        return self.model.show_download_template_as()
 
     def show_create(self):
         return self.model.show_create()
+        
+    def show_create_as(self):
+        return self.model.show_create_as()
 
     def show_edit(self):
         return self.model.show_edit()
+        
+    def show_edit_as(self):
+        return self.model.show_edit_as()
 
     def show_delete(self):
         return self.model.show_delete()
+        
+    def show_delete_as(self):
+        return self.model.show_delete_as()
 
     def show_activate(self):
         return self.model.show_activate()
+        
+    def show_activate_as(self):
+        return self.model.show_activate_as()
 
     def show_deactivate(self):
         return self.model.show_deactivate()
+        
+    def show_deactivate_as(self):
+        return self.model.show_deactivate_as()
 
     def show_approve(self):
         return self.model.show_approve()
+        
+    def show_approve_as(self):
+        return self.model.show_approve_as()
 
     def show_reject(self):
         return self.model.show_reject()
+        
+    def show_reject_as(self):
+        return self.model.show_reject_as()
 
     def get_upload_link(self):
         return self.model.get_upload_link()
