6401b3438f7b88c03b7413d8514479d945233c56
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-11-23 06:03:30 +0000

    base_filter_list_view.py edited online with Bitbucket

--- generics/views/base_filter_list_view.py
+++ generics/views/base_filter_list_view.py
@@ -4,9 +4,12 @@ from generics.views.base_list_view import BaseListView
 class BaseFilterListView(BaseListView):
     paginate_by = 20
     template_name = "base_filter_list_view.html"
+    
+    def get_filter_context(self):
+        return {}
 
-    def get_filter_template(self):
-        return None
+    def get_filter_templates(self):
+        return []
 
     def get_context_data(self, **kwargs):
         context = super(BaseListView, self).get_context_data(**kwargs)
