2d64a868c19fc258047098a22760f3d1b4b8c52a
Md Shariful Islam Sohel <codenginebd@gmail.com>
2017-11-23 09:09:01 +0000

    author_views.py edited online with Bitbucket

--- book_rental/views/author_views.py
+++ book_rental/views/author_views.py
@@ -29,7 +29,7 @@ class AuthorFilterListView(BaseFilterListView):
         if selected_publishers:
             selected_publishers = selected_publishers.split(",")
             selected_publishers = [publisher.strip() for publisher in selected_publishers if publisher]
-        exclude_out_of_stock = request.GET.get('exclude_oos', None)
+        exclude_out_of_stock = request.GET.get('exclude-oos', None)
         if exclude_out_of_stock:
             if exclude_out_of_stock == "1":
                 exclude_out_of_stock = True
@@ -40,6 +40,38 @@ class AuthorFilterListView(BaseFilterListView):
         keyword = request.GET.get('keyword', None)
         if keyword:
             keyword = keyword.strip()
+        rent_available = request.GET.get('rent-available', None)
+        if rent_available:
+            if rent_available == "1":
+                rent_available = True
+            else:
+                rent_available = False
+        else:
+            rent_available = False
+        color_available = request.GET.get('color-available', None)
+        if color_available:
+            if color_available == "1":
+                color_available = True
+            else:
+                color_available = False
+        else:
+            color_available = False
+        ori_available = request.GET.get('ori-available', None)
+        if ori_available:
+            if ori_available == "1":
+                ori_available = True
+            else:
+                ori_available = False
+        else:
+            ori_available = False
+        eco_available = request.GET.get('eco-available', None)
+        if eco_available:
+            if eco_available == "1":
+                eco_available = True
+            else:
+                eco_available = False
+        else:
+            eco_available = False
         return {
             'other_authors': other_authors,
             'all_categories': all_categories,
@@ -47,7 +79,11 @@ class AuthorFilterListView(BaseFilterListView):
             'selected_categories': selected_categories,
             'selected_publishers': selected_publishers,
             'exclude_out_of_stock': exclude_out_of_stock,
-            'keyword': keyword
+            'keyword': keyword,
+            'rent_available': rent_available,
+            'color_available': color_available,
+            'ori_available': ori_available,
+            'eco_available': eco_available
         }
     
     def get_filter_template(self):
