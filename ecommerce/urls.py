bc7c72ccf3c9c45ddfae38283744ac51ff09848c
Md Shariful Islam Sohel <codenginebd@gmail.com>
2018-06-07 19:33:38 +0000

    AddToCartView import changes in ecommerce urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from ecommerce.views.shopping import AddToCartView
+from ecommerce.api.viewsets.add_to_cart_api_view import AddToCartView
 
 urlpatterns = [
     url(r'^add-to-cart/$', AddToCartView.as_view(), name="shopping_add_to_cart_view"),
